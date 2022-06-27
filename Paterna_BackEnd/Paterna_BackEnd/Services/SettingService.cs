2092e3ab5467d27de211a959865416d9e6c0fb5c
GulchinTaghizade <1pylr6h@code.edu.az>
2022-06-27 12:19:37 -0400

    12:19

---
+++ Paterna_BackEnd/Paterna_BackEnd/Services/SettingService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paterna_BackEnd.Services
+{
+    public class SettingService
+    {
+    }
+}
