532e8be98cef02ef399b1f322a92a967a3850df0
GulchinTaghizade <1pylr6h@code.edu.az>
2022-06-27 10:01:57 -0400

    10:01 (Start to Back)

---
+++ Paterna_BackEnd/Paterna_BackEnd/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Paterna_BackEnd.Data;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paterna_BackEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext _context { get; }
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+
+    }
+}
