c8d1f8e8f73b1f81c545b610ada8d0d60f247a03
GulchinTaghizade <1pylr6h@code.edu.az>
2022-06-27 13:05:13 -0400

    13:04

---
+++ Paterna_BackEnd/Paterna_BackEnd/ViewComponents/FooterViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Paterna_BackEnd.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paterna_BackEnd.ViewComponents
+{
+    public class FooterViewComponent : ViewComponent
+    {
+        private ApplicationDbContext _context { get; set; }
+        public FooterViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _context.Settings.ToDictionaryAsync(s => s.Key, s => s.Value);
+            return View(model);
+        }
+    }
+}
---
+++ Paterna_BackEnd/Paterna_BackEnd/ViewComponents/HeaderViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Paterna_BackEnd.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Paterna_BackEnd.ViewComponents
+{
+    public class HeaderViewComponent : ViewComponent
+    {
+        private ApplicationDbContext _context { get; }
+        public HeaderViewComponent(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var model = await _context.Settings.ToDictionaryAsync(s => s.Key, s => s.Value);
+            return View(model);
+        }
+    }
+}
